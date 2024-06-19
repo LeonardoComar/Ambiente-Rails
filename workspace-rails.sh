db5070a98c91c38f76d4795d89bac7791c3213f7
Leonardo Comar <44181591+LeonardoComar@users.noreply.github.com>
2024-06-19 16:24:55 -0300

    Add files via upload

---
+++ workspace-rails.sh
@@ -0,0 +1,45 @@
+echo "iniciando o preparo do ambiente..."
+
+# Atualizacao das dependencias
+sudo apt update
+sudo apt upgrade -y
+sudo apt install curl gpg gcc g++ make -y
+sudo apt-get install -y zip mlocate
+
+# RVM e ruby
+gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
+\curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+\curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
+\curl -sSL https://get.rvm.io | bash -s stable
+echo 'source "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc
+source ~/.bashrc
+rvm use --default --install 3.3.1
+
+# bundler e rails
+gem install bundler
+gem install rails
+
+# postgresql
+sudo apt-get install -y postgresql libpq-dev
+
+# Node.js
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.39.1/install.sh | bash
+source ~/.bashrc
+source ~/.bash_profile
+nvm install 20.14.0
+nvm use 20.14.0
+nvm alias default 20.14.0
+
+# yarn 
+curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
+echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
+sudo apt-get update -y
+sudo apt-get install -y yarn
+
+# git
+sudo apt-get install -y git-all
+
+git config --global user.email "leofcomar@gmail.com"
+git config --global user.name "LeonardoComar"
+
+echo "Ambiente configurado!!"
