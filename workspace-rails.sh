1ce66aa1a878acb2a4a0518b0afdcf97b51a3866
Pedro <130769202+phbrugnolo@users.noreply.github.com>
2025-05-16 17:35:59 -0300

    Update workspace-rails.sh

--- workspace-rails.sh
+++ workspace-rails.sh
@@ -1,41 +1,94 @@
 echo "iniciando o preparo do ambiente..."
 
 # Atualizacao das dependencias
-sudo apt update
-sudo apt upgrade -y
+sudo apt update && sudo apt upgrade -y
 sudo apt install curl gpg gcc g++ make -y
 sudo apt-get install -y zip mlocate
 
 # RVM e ruby
-gpg --keyserver hkp://keyserver.ubuntu.com --recv-keys 409B6B1796C275462A1703113804BB82D39DC0E3 7D2BAF1CF37B13E2069D6956105BD0E739499BDB
-\curl -sSL https://rvm.io/mpapis.asc | gpg --import -
-\curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
+curl -sSL https://rvm.io/mpapis.asc | gpg --import -
+curl -sSL https://rvm.io/pkuczynski.asc | gpg --import -
 \curl -sSL https://get.rvm.io | bash -s stable
-echo 'source "$HOME/.rvm/scripts/rvm"' >> ~/.bashrc
-source ~/.bashrc
-rvm use --default --install 3.3.5
+source ~/.rvm/scripts/rvm
+sudo apt update
+rvm install 3.4.2
+rvm --default use 3.4.2
+
+
+sudo tee /etc/profile.d/rvm.sh << 'EOF'
+#
+# RVM profile
+#
+# /etc/profile.d/rvm.sh # sh extension required for loading.
+#
+
+if
+  [ -n "${BASH_VERSION:-}" -o -n "${ZSH_VERSION:-}" ] &&
+  test "`\command \ps -p $$ -o ucomm=`" != dash &&
+  test "`\command \ps -p $$ -o ucomm=`" != sh
+then
+  [[ -n "${rvm_stored_umask:-}" ]] || export rvm_stored_umask=$(umask)
+
+  # Load user rvmrc configurations, if exist
+  for file in "/etc/rvmrc" "$HOME/.rvmrc"
+  do
+    [[ -s "$file" ]] && source $file
+  done
+  if
+    [[ -n "${rvm_prefix:-}" ]] &&
+    [[ -s "${rvm_prefix}/.rvmrc" ]] &&
+    [[ ! "$HOME/.rvmrc" -ef "${rvm_prefix}/.rvmrc" ]]
+  then
+    source "${rvm_prefix}/.rvmrc"
+  fi
+
+  # Load RVM if it is installed, try user then root install
+  if
+    [[ -s "$rvm_path/scripts/rvm" ]]
+  then
+    source "$rvm_path/scripts/rvm"
+  elif
+    [[ -s "$HOME/.rvm/scripts/rvm" ]]
+  then
+    true ${rvm_path:="$HOME/.rvm"}
+    source "$HOME/.rvm/scripts/rvm"
+  elif
+    [[ -s "/usr/local/rvm/scripts/rvm" ]]
+  then
+    true ${rvm_path:="/usr/local/rvm"}
+    source "/usr/local/rvm/scripts/rvm"
+  fi
+
+  # Add $rvm_bin_path to $PATH if necessary. Make sure this is the last PATH variable change
+  if [[ -n "${rvm_bin_path}" && ! ":${PATH}:" == *":${rvm_bin_path}:"* ]]
+  then PATH="${PATH}:${rvm_bin_path}"
+  fi
+fi
+EOF
+
+sudo chmod +x /etc/profile.d/rvm.sh && source /etc/profile.d/rvm.sh
 
 # bundler e rails
-gem install bundler
 gem install rails
+gem install bundler
+bundle config --global timeout 10000
+bundle config --global retry 10000
 
 # postgresql
 sudo apt-get install -y postgresql libpq-dev
 
 # Node.js
 ## Nota: Toda vez que for instalar o NVM verificar a versão aqui https://github.com/nvm-sh/nvm/releases
-curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.1/install.sh | bash
+curl -o- https://raw.githubusercontent.com/nvm-sh/nvm/v0.40.3/install.sh | bash
 source ~/.bashrc
 source ~/.bash_profile
 nvm install --lts
 nvm use node
 nvm alias default node
 
-# yarn 
-curl -sL https://dl.yarnpkg.com/debian/pubkey.gpg | sudo apt-key add -
-echo "deb https://dl.yarnpkg.com/debian/ stable main" | sudo tee /etc/apt/sources.list.d/yarn.list
-sudo apt-get update -y
-sudo apt-get install -y yarn
+# yarn e npm
+npm install -g npm@latest
+npm install -g yarn --verbose
 
 # git
 sudo apt-get install -y git-all
